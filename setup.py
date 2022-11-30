b8a0ef5671d9582eb1d2c16cb7c4fb50d0c08a8c
Alexandre Iooss <erdnaxe@crans.org>
2022-11-30 10:56:33 +0100

    Update Unicorn to 2.0.1 or later

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 setup(
     name="rainbow",
     install_requires=[
-        "unicorn~=1.0",
+        "unicorn>=2.0.1",
         "capstone>=4.0.0",
         "lief>=0.10.0",
         "intelhex",
